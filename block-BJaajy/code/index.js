1b73769cd169b0e85fca44aae8645b8013ebf8cf
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-03-15 12:12:15 -0700

    done-ish

--- block-BJaajy/code/index.js
+++ block-BJaajy/code/index.js
@@ -51,3 +51,33 @@ Promise.race([promise1, promise2]).then((value) => {
 
 // Question 5
 // It will take the code snippet when executed, 1 second to execute
+
+// Answers
+// Question 1
+let times = [1, 2, 3, 4];
+let timesPromise = times.map(
+  (seconds) =>
+    new Promise((res) => {
+      setTimeout(() => res(Math.random()), seconds * 1000);
+    })
+);
+
+Promise.all(timesPromise).then(console.log);
+
+// Question 2
+// const users = ['getify', 'gaearon', 'AArnott', 'subtleGradient', 'piranha', 'sophiebits'];
+// let userPromises = users.map(
+//   (user) => {
+//       new Promise((res) => {
+//         return fetch(`https://api.github.com/users/${user}`).then((res) => res.json()
+//         )})
+
+// Promise.all(timesPromise).then(console.log)
+
+// Race Question
+
+let promise1 = fetch(`https://random.dog/woof.json`).then((res) => res.json());
+
+let promise2 = fetch(`https://aws.random.cat/meow`).then((res) => res.json());
+
+Promise.race([promise1, promise2]).then(console.log);
