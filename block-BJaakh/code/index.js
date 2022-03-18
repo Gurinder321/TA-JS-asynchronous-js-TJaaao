00e26b456f223b590913bfa2435603866877d1e7
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-03-17 21:12:16 -0700

    done

--- block-BJaakh/code/index.js
+++ block-BJaakh/code/index.js
@@ -12,6 +12,49 @@ function handleDelete(id) {
     displayTodos();
   });
 }
+function handleToggle(id, status) {
+  let data = {
+    todo: {
+      isCompleted: !status,
+    },
+  };
+  fetch(baseURL + `todo/${id}`, {
+    method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }).then(() => {
+    displayTodos();
+  });
+}
+
+function handleEdit(event, id, title) {
+  let input = document.querySelector('input');
+  input.value = title;
+  let p = event.target;
+  let parent = event.target.parentElement;
+  parent.replaceChild(input, p);
+  console.log(input, p, parent);
+  input.addEventListener('keyup', () => {
+    if (event.keyCode === 13 && event.target.value) {
+      let data = {
+        todo: {
+          title: event.target.value,
+        },
+      };
+      fetch(baseURL + `todo/${id}`, {
+        method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      }).then(() => {
+        displayTodos();
+      });
+    }
+  });
+}
 
 function displayUI(data) {
   ulRoot.innerHTML = '';
@@ -19,9 +62,11 @@ function displayUI(data) {
     let li = document.createElement('li');
     let p = document.createElement('p');
     p.innerText = todo.title;
+    p.addEventListener('dblclick', (event) => handleEdit(event, todo._id));
     let input = document.createElement('input');
     input.type = 'checkbox';
     input.checked = todo.isCompleted;
+    input.addEventListener('click', () => handleToggle(todo._id, todo.isCompleted));
     input.setAttribute('data-id', todo._id);
 
     let span = document.createElement('span');
@@ -41,7 +86,7 @@ function displayTodos() {
 }
 
 function addTodo(event) {
-  if (event.keyCode === 13) {
+  if (event.keyCode === 13 && event.target.value.trim()) {
     let data = {
       todo: {
         title: event.target.value,
@@ -55,10 +100,11 @@ function addTodo(event) {
       },
       body: JSON.stringify(data), // body data type must match "Content-Type" header
     }).then(() => {
+      event.target.value = '';
       displayTodos();
     });
   }
 }
 
-todoInput.addEventListener('keyup', () => addTodo);
+todoInput.addEventListener('keyup', addTodo);
 displayTodos();
